2b5fbca52a9f9c76ea2f3d8475dd77eae8d30ed1
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2023-06-06 10:23:25 +0200

    switch
    replaced the if statements by a switch

--- Steenpapierschaar.js
+++ Steenpapierschaar.js
@@ -2,90 +2,93 @@
 const humanOutput = document.querySelector("#human");
 humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze";
 
-const btns = document. querySelectorAll('button');
+// button click event
+const steenBtn = document.querySelector("#steen");
+const papierBtn = document.querySelector("#papier");
+const schaarBtn = document.querySelector("#schaar");
+
+const btns = document.querySelectorAll('button');
 
 let humanChoice = "";
 let computerChoice = "";
 let resultChoice = "";
 
-humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze"
+humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze";
 
 const computerOutput = document.querySelector("#computer");
-const resultOutput = document.querySelector("#result")
-
+const resultOutput = document.querySelector("#result");
 
 function computerChoiceF() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
-  if (randomNumber === 1) {
-    computerChoice = 'steen';
+  switch (randomNumber) {
+    case 1:
+      computerChoice = 'steen';
+      break;
+    case 2:
+      computerChoice = 'schaar';
+      break;
+    case 3:
+      computerChoice = 'papier';
+      break;
   }
-  if (randomNumber === 2) {
-    computerChoice = 'schaar';
-  }
-  if (randomNumber === 3) {
-    computerChoice = 'papier';
-  }
-  computerOutput.innerHTML = computerChoice
+  computerOutput.innerHTML = computerChoice;
 }
 
 function resultChoiceF(humanChoice) {
-  console.log(humanChoice)
-
+  console.log(humanChoice);
 }
-
-
 
 steenBtn.addEventListener("click", function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
   computerChoiceF();
   resultChoiceF('steen');
-  if (computerChoice === 'steen' && humanChoice === 'steen') {
-    resultChoice = "gelijk";
-  }
-  if ( computerChoice === 'papier' && humanChoice === 'steen') {
-    resultChoice = "computer wint";
-    }
-  if ( computerChoice === 'schaar' && humanChoice === 'steen') {
+  switch (true) {
+    case computerChoice === 'steen' && humanChoice === 'steen':
+      resultChoice = "gelijk";
+      break;
+    case computerChoice === 'papier' && humanChoice === 'steen':
+      resultChoice = "computer wint";
+      break;
+    case computerChoice === 'schaar' && humanChoice === 'steen':
       resultChoice = "jij wint";
-    }
-  resultOutput.innerHTML = resultChoice
-
+      break;
+  }
+  resultOutput.innerHTML = resultChoice;
 });
 
 papierBtn.addEventListener("click", function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
   computerChoiceF();
-  if (computerChoice === 'papier' && humanChoice === 'papier') {
-    resultChoice = "gelijk";
+  switch (true) {
+    case computerChoice === 'papier' && humanChoice === 'papier':
+      resultChoice = "gelijk";
+      break;
+    case computerChoice === 'schaar' && humanChoice === 'papier':
+      resultChoice = "computer wint";
+      break;
+    case computerChoice === 'steen' && humanChoice === 'papier':
+      resultChoice = "jij wint";
+      break;
   }
-  if (computerChoice === 'schaar' && humanChoice === 'papier') {
-    resultChoice = "computer wint";
-  }
-  if (computerChoice === 'steen' && humanChoice === 'papier') {
-    resultChoice = "jij wint";
-  }
-
-  resultOutput.innerHTML = resultChoice
-
+  resultOutput.innerHTML = resultChoice;
 });
 
 schaarBtn.addEventListener("click", function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
   computerChoiceF();
-  if (computerChoice === 'steen' && humanChoice === 'schaar') {
-    resultChoice = "computer wint";
+  switch (true) {
+    case computerChoice === 'steen' && humanChoice === 'schaar':
+      resultChoice = "computer wint";
+      break;
+    case computerChoice === 'papier' && humanChoice === 'schaar':
+      resultChoice = "jij wint";
+      break;
+    case computerChoice === 'schaar' && humanChoice === 'schaar':
+      resultChoice = "gelijk";
+      break;
   }
-  if (computerChoice === 'papier' && humanChoice === 'schaar') {
-    resultChoice = "jij wint";
-  }
-  if (computerChoice === 'schaar' && humanChoice === 'schaar') {
-    resultChoice = "gelijk";
-  }
-
-  resultOutput.innerHTML = resultChoice
+  resultOutput.innerHTML = resultChoice;
 });
-
-
